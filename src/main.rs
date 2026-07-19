f759cccc4c4b58dc14aa450b06874dde6bdf56d1
Ryota Sakamoto <sakamo.ryota@gmail.com>
2026-07-19 17:31:29 +0900

    use alloca

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ fn main() {
         panic!("args should be specified.");
     }
 
-    let num: u64 = args[1].parse().unwrap();
+    let p = &args[1];
 
     println!("define i32 @main(i32, i8**) {{");
-    println!("  ret i32 {}", num);
+    println!("  %3 = alloca i32");
+    println!("  store i32 {}, ptr %3", p);
+    println!("  %4 = load i32, ptr %3");
+    println!("  ret i32 %4");
     println!("}}");
 }
