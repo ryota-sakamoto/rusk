692737fe500cbd53aa9acce0fcdd38ca303ea5a2
Ryota Sakamoto <sakamo.ryota@gmail.com>
2026-07-19 16:33:59 +0900

    generate simple llvm ir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        panic!("args should be specified.");
+    }
+
+    let num: u64 = args[1].parse().unwrap();
+
+    println!("define i32 @main(i32, i8**) {{");
+    println!("  ret i32 {}", num);
+    println!("}}");
 }
